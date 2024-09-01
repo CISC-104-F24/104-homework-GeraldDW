dc4d1809618c6ef48db86f138a60b95b21d0a524
GeraldDW <geralddw@udel.edu>
2024-09-01 19:35:29 -0400

    Update Hello World

--- Homework Project/Assets/Scripts/HelloWorld.cs
+++ Homework Project/Assets/Scripts/HelloWorld.cs
@@ -6,8 +6,17 @@ public class HelloWorld : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-     Debug.Log ("Hello, World");   
+    { 
+     Debug.LogWarning ("Hello,I am a warning");
+     Debug.Log ("Hello, world!");
+     Debug.LogError("Hello, I am an error");
+     //Author: Gerald Williamson
+
+     /*
+     *I want to know how companies go back
+     * and edit the code
+     * when the game is released
+     */
     }
 
     // Update is called once per frame
